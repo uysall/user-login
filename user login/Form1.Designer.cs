129186395157cdc9fab5600407829274bcf211d5
Ömer Uysal <omersadikuysal@gmail.com>
2024-07-25 13:34:47 +0300

    Proje dosyası ekle.

---
+++ user login/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using static System.Windows.Forms.LinkLabel;
+using System.Windows.Forms;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Lifetime;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace user_login
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposing || components == null)
+            {
+                return;
+            }
+            components.Dispose();
+            class Program
+        {
+            static void Main()
+            {
+                string savedUsername = "admin";
+                string savedPassword = "12345";
+
+                Console.WriteLine("Kullanıcı Adı:");
+                string username = Console.ReadLine();
+
+                Console.WriteLine("Şifre:");
+                string password = Console.ReadLine();
+
+                if (username == savedUsername && password == savedPassword)
+                {
+                    Console.WriteLine("Giriş başarılı!");
+
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adı veya şifre yanlış!");
+                }
+
+            }
+        }
+    }
+}
+
+
---
+++ user login/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace user_login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+          
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(622, 356);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
